a518c5add240db3fee0b26a78da6d11a9deec9e6
Meghan Domeck <mdomeck@aol.com>
2020-09-14 17:42:14 -0400

    up and running

---
+++ basics/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+  public static void main(String[] args) {
+
+    String name = pluralize("hi", 5);
+    System.out.println(name);
+  }
+
+
+public static String pluralize(String word, int num){
+  
+  if (num > 0)
+  System.out.println("Hello World");
+  return "hello world";
+}
+}
